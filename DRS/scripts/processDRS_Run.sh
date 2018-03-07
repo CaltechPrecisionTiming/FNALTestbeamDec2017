56f4e13617c88f975449afb5e95ee37c3b136e7d
Christopher Rogan <crogan@Christophers-iMac-2.local>
2018-03-06 18:44:10 -0600

    test batch submission

--- DRS/scripts/processDRS_Run.sh
+++ DRS/scripts/processDRS_Run.sh
@@ -14,7 +14,7 @@ echo "Processing DRS/Pixel data for run ${runNum} in location ${output}"
 FILES=$(eos ls root://cmseos.fnal.gov//store/user/cmstestbeam/ETL/MT6Section1Data/122017/OTSDAQ/CMSTiming/ | grep RawDataSaver0CMSVMETiming_Run${runNum})
 for f in $FILES
 do
-    xrdcp root://cmseos.fnal.gov//store/user/cmstestbeam/ETL/MT6Section1Data/122017/OTSDAQ/CMSTiming/${f} > ${runNum}.log
+    xrdcp root://cmseos.fnal.gov//store/user/cmstestbeam/ETL/MT6Section1Data/122017/OTSDAQ/CMSTiming/${f}
 done
 ls * > ${runNum}.log
 xrdcp ${runNum}.log root://cmseos.fnal.gov/${output}
